29dc4ae3baf3e79001970dbd98d4043291d08cb5
Daria Gavrish <121836323+GavrishD@users.noreply.github.com>
2023-02-16 16:08:49 +0200

    Update script_3.js

--- lesson_3/js/script_3.js
+++ lesson_3/js/script_3.js
@@ -11,12 +11,13 @@ let randomNumber = Math.floor(Math.random() * 5) + 1
 //3. Виведення результату
 if (numberUser === randomNumber)
 document.write(`Ура! Ви вгадали число :)`)
-else (numberUser != randomNumber)
+else {
 alert(`Жаль, але ви не вгадали число. Залишилася одна спроба.`)
 
 let numUser = parseInt(prompt('Введіть число від 1 до 5. У вас дві спроби вгадати число.', '1'))
 
 if (numUser === randomNumber)
 document.write(`Ура! Ви вгадали число :)`)
-else (numUser != randomNumber)
+else 
 document.write(`Жаль, але ви не вгадали число. Ваші числа - ${numberUser} і ${numUser}. Рандомне число - ${randomNumber}. Спроби закінчились.`)
+}
