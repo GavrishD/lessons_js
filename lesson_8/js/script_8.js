e9b64e3c88dba9ac5390a4011d671a0ce1f85df7
Daria Gavrish <121836323+GavrishD@users.noreply.github.com>
2023-03-07 01:39:53 +0200

    Update script_8.js

--- lesson_8/js/script_8.js
+++ lesson_8/js/script_8.js
@@ -1,14 +1,30 @@
-const imageYou = `img/youtube.png`
-const headerTitle = 'Переходь на YouTube'
-const websiteLink = 'https://www.youtube.com'
-
-const bannerYou = mediaBanner(imageYou, headerTitle, websiteLink)
-
-function mediaBanner(imageYou, headerTitle = '', websiteLink ='#') {
-    const bannerHeader = `<h3>${headerTitle}</h3>`
-    const bannerContent = `<img src='${imageYou}' alt='Banner'>`
-    const bannerYou = `<a href='${websiteLink}'>${bannerContent}${bannerHeader}</a>`
-    return bannerYou
+// Масив виграшу
+let numberWin = 4
+function getAllWins(numberWin) {
+    let sum = []
+    for (let i = 0; i < numberWin; i++) {
+        const sumWin = -500 + Math.floor(Math.random() * (500 - -500 +1))
+        sum.push(sumWin)
+    }
+    return sum
 }
+let allWins = getAllWins(numberWin)
 
-document.write(`${bannerYou}`)
+
+// Виграшна сума гравця
+function getSumUser(winUser) {
+    let sumWin = 0
+    let inform = 0
+    for (let i = 0; i <= winUser.length; i++) {
+        const choiceUser = parseInt(prompt(`Введіть номер вид 0 до ${allWins.length - 1}`))
+        sumWin += winUser[choiceUser]
+        const playInfo = confirm('Бажаете продовжувати?')
+        if (playInfo === true) inform++
+        else break
+    }
+    return sumWin
+}
+let sumUser = getSumUser(allWins)
+
+
+document.write(`Виграшні суми - ${allWins}<br>Ваш виграш - ${sumUser}`)
