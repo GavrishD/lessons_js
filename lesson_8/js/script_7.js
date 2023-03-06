0c84a80c642036333f14600bf6efa2dc6001db38
Daria Gavrish <121836323+GavrishD@users.noreply.github.com>
2023-03-07 01:39:28 +0200

    Update script_7.js

--- lesson_8/js/script_7.js
+++ lesson_8/js/script_7.js
@@ -1,28 +1,49 @@
-const randomPicture = getImages(
-    'img/01.png',
-    'img/02.png',
-    'img/03.png',
-    'img/04.png'
-)
-
-function getImages(image1, image2, image3, image4) {
-    const randomImage = Math.floor(Math.random() * 4)
-    let result 
-
-    switch (randomImage) {
-        case 0: 
-            result = image1
-            break
-        case 1: 
-            result = image2
-            break
-        case 2: 
-            result = image3
-            break
-        case 3: 
-            result = image4
+// Платежі за рік
+let monthOfYears = 12
+function getPay(numMonths) {
+    let allPay = []
+    for (let i = 0; i < numMonths; i++) {
+        const pay = 10 + Math.floor(Math.random() * 1000)
+        allPay.push(pay)
     }
-    return `<img src='${result}' alt='image ${randomImage}'`;
+    return allPay
 }
+let allPayments = getPay(monthOfYears)
 
-document.write(`${randomPicture}`)
+// Кількість грошей за період
+function getAmountMoney(paymentAll, payStart, payEnd) {
+    let sumMoney = 0
+    for (let i = payStart; i <= payEnd; i++) {
+        sumMoney += paymentAll[i - 1]
+    }
+    return sumMoney
+}
+let amountYear = getAmountMoney(allPayments, 1, 12)
+let amountFirstHalf = getAmountMoney(allPayments, 1, 6)
+let amountSecondHalf = getAmountMoney(allPayments, 7, 12)
+let amountSummer = getAmountMoney(allPayments, 6, 8)
+let amountQuarter = getAmountMoney(allPayments, 4, 6)
+
+
+// Кількість грошей за декілька місяців
+function getAmountMonths(allPaymant) {
+    let sum = allPaymant[2] + allPaymant[5] + allPaymant[8] + allPaymant[11]
+    return sum
+}
+let amountMonths = getAmountMonths(allPayments)
+let amountFirstSpring = getAmountMonths(allPayments, 3, 3)
+let amountFirstSummer = getAmountMonths(allPayments, 6, 6);
+
+
+// Кількість грошей за парні місяці
+function getPairedMonths(allPaymant) {
+    let sum = 0
+    for (let i = 1; i <= allPaymant.length; i++) {
+        if (i % 2 === 0) sum += allPaymant[i - 1]
+    }
+    return sum
+}
+let pairedMonths = getPairedMonths(allPayments)
+
+
+document.write(`Платежі протягом року - ${allPayments}<br>Сума платежів за рік - ${amountYear}<br>Сума платежів за першу половину року - ${amountFirstHalf}<br>Сума платежів за другу половину року - ${amountSecondHalf}<br>Сума платежів за літо - ${amountSummer}<br>Сума платежів за ІІ квартал - ${amountQuarter}<br>Сума платежів за місяці, які є початковими - весна, літо, осінь, зима -> ${amountMonths}<br>Сума платежів за місяць, який є початковим - весна -> ${amountFirstSpring}<br>Сума платежів за місяць, який є початковим - літо -> ${amountFirstSummer}<br>Сума платежів за парні місяці - ${pairedMonths}`)
