c270e0687669e28bfae181e8f257178eb5b1d5f3
Daria Gavrish <121836323+GavrishD@users.noreply.github.com>
2023-03-07 01:40:12 +0200

    Update script_9.js

--- lesson_8/js/script_9.js
+++ lesson_8/js/script_9.js
@@ -1,18 +1,30 @@
-function getMinusTemperature() {
-    let numberTemperature = parseInt(prompt('Введіть кількість показників, які ви маєте', '1'))
-    let sumIndicators = 0
+let cellSet = parseInt(prompt('Введіть кількість клітинок поля', '8'))
+let shipSet = parseInt(prompt('Введіть кількість кораблів', '2'))
 
-    for (let i = 1; i <= numberTemperature; i++) {
-        let indicatorTemperature = parseFloat(prompt(`Введіть температуру показників ${i}`))
-        let enumerator = 0
-
-        if(indicatorTemperature < 0)
-        enumerator = enumerator + 1
-        sumIndicators = sumIndicators + enumerator
+// Поле для гри 
+function getFieldPlay(numCellSet, numShipSet) {
+    let field = []
+    for (let i = 0; i < numCellSet; i++) {
+        field.push(0)
     }
-        document.write(`Кількість відʼємних показників температури -> ${sumIndicators}`)
-    return numberTemperature
-    
+    for (let i = 0; i < numShipSet; ){
+    const numRandom = 1 + Math.floor(Math.random() * numCellSet)
+    if (field[numRandom - 1] === 0) field[numRandom - 1] = 1;
+    numShipSet--
+    }
+    return field
+}
+let fieldPlay = getFieldPlay(cellSet, shipSet)
+
+// Цикл гри
+for (let i = 0; i < shipSet; ) {
+    let playerShot = parseInt(prompt(`Введіть клітинку для вистрелу від 1 до ${cellSet}`, '1'))
+    if (fieldPlay[playerShot - 1] === 1) {
+        shipSet--
+        alert(`Ви вбили корабель, залишилось ${shipSet}`)
+    } else 
+        alert(`Ви не потрапили, залишилось ${shipSet}`);
 }
 
-const sumIndicators = getMinusTemperature()
+
+document.write(`Масив з кораблями - ${fieldPlay}`);
